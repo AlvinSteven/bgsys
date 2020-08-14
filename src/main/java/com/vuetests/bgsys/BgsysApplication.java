23fda97c739d4ab5a40d3925cc948a22dc4ac975
AlvinSteven <53334961+AlvinSteven@users.noreply.github.com>
2020-08-14 12:43:55 +0800

    new version-by alvin

--- src/main/java/com/vuetests/bgsys/BgsysApplication.java
+++ src/main/java/com/vuetests/bgsys/BgsysApplication.java
@@ -2,11 +2,13 @@ package com.vuetests.bgsys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import io.sentry.Sentry;
 
 @SpringBootApplication
 public class BgsysApplication {
 
     public static void main(String[] args) {
+//        Sentry.init("https://public:private@host:port/1");
         SpringApplication.run(BgsysApplication.class, args);
     }
 
